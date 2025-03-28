1fe36eb094a67a4d68fa7671efb04ab8018d3677
Adi Wijaya <adiwijaya5699@gmail.com>
2025-03-28 08:02:18 +0700

    first build

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,19 @@ import Todos from '@/components/todos';
 import { getAllUsers } from '@/actions/userActions';
 
 export default async function Home() {
-  // const users = await getAllUsers();
-  // console.log(users);
-  // const data = await getData(users[0].id);
-  // const user = await getData(users[0].id);
+  const users = await getAllUsers();
+  console.log(users);
+  const data = await getData(users[0].id);
+  const user = await getData(users[0].id);
 
-  // console.log(user);
+  console.log(user);
 
   return (
     <main className="flex items-center justifay-between">
-      {/* <Todos
+      <Todos
         todos={data}
         user={users[0]}
-      /> */}
+      />
     </main>
   );
 }
